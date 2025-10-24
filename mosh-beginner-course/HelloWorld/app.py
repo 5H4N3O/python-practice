26e689ce13d52ed582195f908a608c95ed0aaf78
5H4N3O <shanepratt2002@gmail.com>
2025-10-24 14:34:15 -0500

    Learned about python strings and some methods

--- mosh-beginner-course/HelloWorld/app.py
+++ mosh-beginner-course/HelloWorld/app.py
@@ -1,2 +1,7 @@
 print('Hello World')
 print('*' * 10)
+print("hello world")
+2 + 3
+x = 1
+y = 2
+unit_price = 3
---
+++ mosh-beginner-course/strings.py
@@ -0,0 +1,59 @@
+course = 'Python Programming'
+
+# len Returns number of chars in string
+print(len(course))
+
+# Use [] and : to slice strings
+# [] returns the i+1th char of the string
+print(course[0])
+
+# The first char starting from the end
+print(course[-1])
+
+# Chars 0-3
+print(course[0:3])
+
+# Chars 0-end
+print(course[0:])
+
+# Chars before 3
+print(course[:3])
+
+print(course[:])
+
+# Escape Character (\)
+# \'
+# \\
+# \n
+course = "Python \nProgramming"
+print(course)
+
+# Format Strings f''
+first = 'Shane'
+last = 'Pratt'
+
+full = first + ' ' + last
+print(full)
+
+full = f'{first} {last}'
+print(full)
+
+full = f'{len(first)} {last}'
+print(full)
+
+full = f'{len(first)} {2 + 2}'
+print(full)
+
+# String methods
+course = '    python programming    '
+course_capital = course.upper()
+print(course_capital)
+print(course.lower())
+print(course.title())
+print(course.strip())
+print(course.lstrip())
+print(course.rstrip())
+print(course.find('pro'))
+print(course.replace('p', 'j'))
+print('pro' in course)
+print('swift' not in course)
