26ee3eb48e0e1a810600e14fb515bdbdab88febd
5H4N3O <shanepratt2002@gmail.com>
2025-10-27 10:13:20 -0500

    Completed numbers lesson
    Learned more about numbers in python and some of their methods

---
+++ mosh-beginner-course/numbers.py
@@ -0,0 +1,23 @@
+import math
+
+x = 1       # ints
+x = 1.1     # floats
+x = 1 + 2j  # a + bi, imaginary numbers
+
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)   # float division
+print(10 // 3)  # integer division
+print(10 % 3)   # modulus: remainder of 10/3
+print(10 ** 3)  # Exponent: 10^3
+
+x = 10
+x = x + 3
+x += 3  # shorcut for x = x + 3
+
+###### Working with numbers: #####
+print(round(2.9))
+print(abs(-2.9))
+
+print(math.ceil(2.2))
